25b4ba4cd9eb34ab8079d5ad44c1c29b5a61d8e9
shabu529393 <mshoque5293@gmail.com>
2024-12-25 23:11:58 +0600

    learn ES6

---
+++ chain.js
@@ -0,0 +1,17 @@
+// data access 
+
+const data = [{id: 1, name: 'abul', address: 'kochu khet'}];
+
+console.log(data[0].address);
+
+const products = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'lenovo laptop', price: 65000},
+        {id: 2, name: 'macbook', price: 185000},
+
+    ]
+}
+
+// need second product price 
+console.log(products.data[1].price)
---
+++ filter.js
@@ -0,0 +1,11 @@
+
+// filter selects elements based on a condition and returns an array with the element that fulfilled the condition
+const numbers = [1,5,6,4,15];
+
+const players = [75, 65, 67, 72, 55, 59];
+
+// const selected = players.filter(p => p>70);
+// const selected = players.filter(p => p>80);
+// const selected = players.filter(p => p>50);
+const selected = players.filter(p => p%2===1);
+console.log(selected);
